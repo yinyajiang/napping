1d1fcbfa02a1f756acbe473ed6abd95cefd87af5
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2022-06-14 17:24:53 +0800

    add StreamPayload and NotProcessBody option

--- request.go
+++ request.go
@@ -36,7 +36,7 @@ type Request struct {
 	Params  *url.Values // URL query parameters
 	Payload interface{} // Data to JSON-encode and POST
 
-	// Can be set to true if Payload is of type io.Reader and client
+	// Can be set to true if Payload is of type io.Reader and  client
 	StreamPayload bool
 
 	// Not capture response body and unmarshaled
